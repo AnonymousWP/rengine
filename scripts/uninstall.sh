6ecd174bf50ecf76ad33723e36f25b22426d1b78
AnonymousWP <50231698+AnonymousWP@users.noreply.github.com>
2023-10-12 20:13:00 +0200

    ops(`uninstall.sh`): add echo messages

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -17,17 +17,19 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
   echo "Stopping reNgine"
   docker stop rengine-web-1 rengine-db-1 rengine-celery-1 rengine-celery-beat-1 rengine-redis-1 rengine-tor-1 rengine-proxy-1
-  
+  echo "Stopped reNgine"
+
   echo "Removing all containers related to reNgine"
   docker rm rengine-web-1 rengine-db-1 rengine-celery-1 rengine-celery-beat-1 rengine-redis-1 rengine-tor-1 rengine-proxy-1
-  echo "Removed all containers"
+  echo "Removed all containers related to reNgine"
 
   echo "Removing all volumes related to reNgine"
   docker volume rm rengine_gf_patterns rengine_github_repos rengine_nuclei_templates rengine_postgres_data rengine_scan_results rengine_tool_config rengine_static_volume rengine_wordlist
-  echo "Removed all volumes"
+  echo "Removed all volumes related to reNgine"
 
   echo "Removing all networks related to reNgine"
   docker network rm rengine_rengine_network rengine_default
+  echo "Removed all networks related to reNgine"
 else
   exit 1
 fi
